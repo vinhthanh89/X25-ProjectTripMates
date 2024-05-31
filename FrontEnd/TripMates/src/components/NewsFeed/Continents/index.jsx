5b0fb32ba7af77e51cbcf41991a609f1396d93af
iamchouty <quocchau4729@gmail.com>
2024-05-31 15:57:16 +0700

    FrontEnd: left + right side bar fixed, added header to newsfeed

--- FrontEnd/TripMates/src/components/NewsFeed/Continents/index.jsx
+++ FrontEnd/TripMates/src/components/NewsFeed/Continents/index.jsx
@@ -6,88 +6,50 @@ import {
   FaGlobe,
 } from "react-icons/fa";
 import { FaEarthOceania } from "react-icons/fa6";
+
 const Continents = () => {
+  const continents = [
+    { icon: FaGlobe, name: "Others..." },
+    { icon: FaGlobeAfrica, name: "Africa" },
+    { icon: FaEarthOceania, name: "Oceania" },
+    { icon: FaGlobeEurope, name: "Europe" },
+    { icon: FaGlobeAsia, name: "Asia" },
+    { icon: FaGlobeAmericas, name: "Americas" },
+  ];
+
   const iconSize = 50;
   const iconColor = { color: "#e6e6e64d" };
+
   return (
-    <>
-      <div className="continents flex justify-between">
-        <div className="dropdown dropdown-hover dropdown-end dm-sans">
-          <div tabIndex={0} className="flex items-center hover:cursor-pointer">
-            <p className="font-bold p-2 rounded-lg hover:bg-black hover:text-white transition duration-300 ease-in-out">
-              Continents
-            </p>
-          </div>
-          <div
-            tabIndex={0}
-            className="menu dropdown-content z-[1] p-4 bg-white shadow-xl rounded-xl border-[1px] border-[#cccccc]"
-          >
-            <div className="grid grid-rows-2 grid-cols-3 w-[40rem] gap-[2rem]">
-              <button className="p-5 flex flex-col items-center gap-[5px] bg-black rounded-lg hover_trans hover:scale-105">
-                <FaGlobe
+    <div className="continents flex justify-between">
+      <div className="dropdown dropdown-hover dropdown-end dm-sans">
+        <div tabIndex={0} className="flex items-center hover:cursor-pointer">
+          <p className="font-bold p-2 rounded-lg hover:bg-black hover:text-white transition duration-300 ease-in-out">
+            Continents
+          </p>
+        </div>
+        <div className="menu dropdown-content z-[1] p-4 bg-white shadow-xl rounded-xl border-[1px] border-[#cccccc]">
+          <div className="grid grid-rows-2 grid-cols-3 w-[40rem] gap-[2rem]">
+            {continents.map(({ icon: Icon, name }) => (
+              <button
+                key={name}
+                className="p-5 flex flex-col items-center gap-[15px] bg-black rounded-lg hover_trans hover:scale-105"
+              >
+                <Icon
                   size={iconSize}
                   style={iconColor}
                   className="bg-transparent"
                 />
-                <p className="bg-transparent text-white font-semibold text-xl">
-                  Other...
+                <p className="bg-transparent text-white font-semibold text-lg">
+                  {name}
                 </p>
               </button>
-              <button className="p-5 flex flex-col items-center gap-[5px] bg-black rounded-lg hover_trans hover:scale-105">
-                <FaGlobeAfrica
-                  size={iconSize}
-                  style={iconColor}
-                  className="bg-transparent"
-                />
-                <p className="bg-transparent text-white font-semibold text-xl">
-                  Africa
-                </p>
-              </button>
-              <button className="p-5 flex flex-col items-center gap-[5px] bg-black rounded-lg hover_trans hover:scale-105">
-                <FaEarthOceania
-                  size={iconSize}
-                  style={iconColor}
-                  className="bg-transparent"
-                />
-                <p className="bg-transparent text-white font-semibold text-xl">
-                  Oceania
-                </p>
-              </button>
-              <button className="p-5 flex flex-col items-center gap-[5px] bg-black rounded-lg hover_trans hover:scale-105">
-                <FaGlobeEurope
-                  size={iconSize}
-                  style={iconColor}
-                  className="bg-transparent"
-                />
-                <p className="bg-transparent text-white font-semibold text-xl">
-                  Europe
-                </p>
-              </button>
-              <button className="p-5 flex flex-col items-center gap-[5px] bg-black rounded-lg hover_trans hover:scale-105">
-                <FaGlobeAsia
-                  size={iconSize}
-                  style={iconColor}
-                  className="bg-transparent"
-                />
-                <p className="bg-transparent text-white font-semibold text-xl">
-                  Asia
-                </p>
-              </button>
-              <button className="p-5 flex flex-col items-center gap-[5px] bg-black rounded-lg hover_trans hover:scale-105">
-                <FaGlobeAmericas
-                  size={iconSize}
-                  style={iconColor}
-                  className="bg-transparent"
-                />
-                <p className="bg-transparent text-white font-semibold text-xl">
-                  Americas
-                </p>
-              </button>
-            </div>
+            ))}
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
+
 export default Continents;
--- FrontEnd/TripMates/src/components/NewsFeed/SideLeft/index.jsx
+++ FrontEnd/TripMates/src/components/NewsFeed/SideLeft/index.jsx
@@ -1,6 +1,6 @@
 
 import { BiSolidMoviePlay } from "react-icons/bi";
-import { FaCompass, FaUser } from "react-icons/fa";
+import { FaCompass, FaUser, FaSignOutAlt } from "react-icons/fa";
 import { IoIosMail, IoIosSettings } from "react-icons/io";
 import { useSelector } from "react-redux";
 
@@ -11,21 +11,20 @@ const LeftSideBar = () => {
   const iconStyle = { background: "transparent" };
   const user = useSelector((state) => state.user.user);
 
-
   return (
     <>
-      <div className="flex flex-col text-black font-bold gap-[1rem] w-[85%] h-[40rem] pt-[2rem]">
-        <div className="flex justify-center items-center gap-[5px]">
-          <img src="./tripmates-favicon-color.png" className="w-[3rem]" />
-          <h1 className="text-2xl text-black font-semibold">TripMates</h1>
-        </div>
+      <div className="flex flex-col text-black font-bold gap-[3rem] h-[40rem] pt-[2rem] w-[85%]">
         <div className="flex flex-col gap-8">
           <div className="flex flex-col items-center">
-          <img
-          className="w-[100px] h-[100px] object-cover rounded-full"
-           src={user.avatar} alt="" />
+            <img
+              className="w-[100px] h-[100px] object-cover rounded-full"
+              src={user.avatar}
+              alt=""
+            />
             {/* <BiSolidUserCircle size={100} /> */}
-            <h1 className="text-2xl text-center text-black font-bold">{user.fullName}</h1>
+            <h1 className="text-2xl text-center text-black font-bold">
+              {user.fullName}
+            </h1>
             <p className="text-[#717171] text-center text-sm font-normal">
               {user.email}
             </p>
@@ -63,20 +62,20 @@ const LeftSideBar = () => {
               />
               Messages
             </button>
-            <button className="flex justify-start items-center btn_all gap-2 w-full focus:bg-black focus:text-white hover:bg-black hover:text-white transition duration-300 ease-in-out">
-              <IoIosSettings
+            <button className="flex justify-start items-center btn_all gap-2 w-full hover:bg-black hover:text-white transition duration-300 ease-in-out">
+              <FaSignOutAlt
                 className="group-hover:bg-black"
                 size={iconSize}
                 style={iconStyle}
               />
-              Settings
-            </button>
-          </div>
-          <div className="flex justify-center pt-8">
-            <button className="text-white btn_all w-[70%] hover:bg-red-700">
               Sign out
             </button>
           </div>
+          {/* <div className="flex justify-center pt-8">
+            <button className="bg-red-500 text-white btn_all w-[70%] hover:bg-red-700">
+              Sign out
+            </button>
+          </div> */}
         </div>
       </div>
     </>
