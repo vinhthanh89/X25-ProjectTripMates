236152abb07b381071cde5edc83e577e37e63c10
iamchouty <quocchau4729@gmail.com>
2024-06-03 21:55:01 +0700

    FrontEnd - Added Modal change password

--- FrontEnd/TripMates/src/components/NewsFeed/SideLeft/index.jsx
+++ FrontEnd/TripMates/src/components/NewsFeed/SideLeft/index.jsx
@@ -1,73 +1,43 @@
 import { BiSolidMoviePlay } from "react-icons/bi";
 import { FaCompass, FaSignOutAlt } from "react-icons/fa";
 import { AiFillMessage } from "react-icons/ai";
-import { RiLockPasswordFill } from "react-icons/ri";
-
-import { useDispatch, useSelector } from "react-redux";
-import { logoutAction } from "../../../features/user/userSlices";
-import { useNavigate } from "react-router";
+import User from "./User";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { getUserById } from "../../../services/user";
 
 const LeftSideBar = () => {
   const iconSize = 20;
   const iconStyle = { background: "transparent" };
+ 
+  const [userProfile, setUserProfile] = useState({
+    email: "",
+    fullName: "",
+    avatar: "",
+    age: null,
+    birthday: "",
+    gender: "",
+    description: "",
+  });
+  const urlParam = useParams();
 
-
-  const user = useSelector((state) => state.user.user);
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const handleLogOut = () => {
-    dispatch(logoutAction())
-    navigate('/')
-  }
-
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await getUserById(urlParam.userId);
+        setUserProfile(response.data.findUser);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUserData();
+  }, [urlParam.userId, userProfile]);
   return (
     <>
       <div className="flex flex-col font-bold gap-[3rem] pt-[2rem] w-[85%]">
         <div className="flex flex-col gap-12">
           <div className="flex flex-col gap-5">
-            <div className="dropdown dropdown-right dropdown-hover">
-              <div
-                tabIndex={0}
-                role="button"
-                className="bg-white flex items-center gap-2 cursor-pointer btn_all hover:bg-[#e6e6e6] focus:bg-[#e6e6e6]"
-              >
-                <div className="w-[22px] h-[22px]">
-                  <img
-                    className="w-full h-full object-cover rounded-full"
-                    src={user.avatar}
-                    alt=""
-                  />
-                </div>
-                <div className="flex-1 overflow-hidden">
-                  <p className="truncate">{user.fullName}</p>
-                  {/* <p className="text-xs text-[#717171]">{user.email}</p> */}
-                </div>
-              </div>
-              <ul
-                tabIndex={0}
-                className="dropdown-content z-[1] menu p-4 shadow bg-white rounded-box w-52 gap-3 "
-              >
-                <button className="flex justify-start items-center btn_all gap-2 w-full hover:bg-[#e6e6e6] transition duration-300 ease-in-out text-sm">
-                  <RiLockPasswordFill
-                    className=""
-                    size={iconSize}
-                    style={iconStyle}
-                  />
-                  <p className="">Change password</p>
-                </button>
-                <button
-                  className="flex justify-start items-center btn_all gap-2 w-full transition duration-300 ease-in-out bg-red-600 hover:bg-red-500 text-white text-sm"
-                  onClick={handleLogOut}
-                >
-                  <FaSignOutAlt
-                    className=""
-                    size={iconSize}
-                    style={iconStyle}
-                  />
-                  <p className="">Sign out</p>
-                </button>
-              </ul>
-            </div>
+            <User userPassword={userProfile} />
             <button className="flex justify-start items-center btn_all gap-2 w-full focus:bg-[#303030] focus:text-white hover:bg-[#303030] hover:text-white transition duration-300 ease-in-out">
               <FaCompass
                 className="group-hover:bg-[#303030]"
