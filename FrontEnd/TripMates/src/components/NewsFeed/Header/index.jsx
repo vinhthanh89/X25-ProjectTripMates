515bb703fde782b03c559e8d5e05247f8e5f251b
iamchouty <quocchau4729@gmail.com>
2024-06-04 23:10:55 +0700

    FrontEnd - Fixed layout

--- FrontEnd/TripMates/src/components/NewsFeed/Header/index.jsx
+++ FrontEnd/TripMates/src/components/NewsFeed/Header/index.jsx
@@ -1,25 +1,35 @@
-// import { IoIosNotificationsOutline } from "react-icons/io";
-
+import { IoIosNotificationsOutline, IoIosSettings } from "react-icons/io";
+import { FaSignOutAlt } from "react-icons/fa";
+import { logoutAction } from "../../../features/user/userSlices.js";
 import { useNavigate } from "react-router";
-
-// import { FaUserFriends } from "react-icons/fa";
+import ModalChangePassword from "../../UserProfile/ModalChangePassword";
+import { useDispatch, useSelector } from "react-redux";
 const Header = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user.user);
+  const dispatch = useDispatch();
+  const handleLogOut = () => {
+    dispatch(logoutAction());
+    navigate("/");
+  };
+  const iconSize = 30;
+  const iconStyle = { background: "transparent" };
 
-
-  // const iconSize = 30;
   return (
     <>
       <div className="grid grid-cols-5 p-[0.8rem] w-full border-b-[1.5px]">
-        <button onClick={() => navigate('/')} className="col-span-1 flex items-center gap-[5px]">
+        <button
+          onClick={() => navigate("/")}
+          className="col-span-1 flex items-center gap-[5px]"
+        >
           <img
             src="../../../../public/tripmates-favicon-black.png"
             className="w-[3rem]"
           />
           <h1 className="text-2xl font-semibold text-[#5143d9]">TripMates</h1>
         </button>
-        <div className="col-span-3 flex justify-center">
-          <label className="input input-bordered flex items-center gap-2 bg-white w-[60%]">
+        <div className="col-span-3 flex justify-center gap-[25px]">
+          <label className="input input-bordered flex items-center gap-2 bg-white w-[65%]">
             <input
               type="text"
               className="grow text-black font-semibold"
@@ -38,19 +48,39 @@ const Header = () => {
               />
             </svg>
           </label>
+          <button className="font-bold bg-[#5143d9] text-white p-3 rounded-xl">
+            Start a new trip
+          </button>
         </div>
-        {/* <div className="col-span-1 flex px-[2rem] gap-[1rem]">
-          <button className="btn_all bg-white text-black hover:scale-105">
+        <div className="col-span-1 flex justify-end gap-[0.5rem]">
+          <button className="btn_all  text-[#545454] hover:scale-105">
             <IoIosNotificationsOutline
               size={iconSize}
               className="bg-transparent"
             />
           </button>
-          <button className="btn_all flex items-center justify-between bg-white text-black gap-1 w-[8rem] hover:scale-105">
-            <FaUserFriends size={iconSize} className="bg-transparent" />
-            <p>Friends</p>
-          </button>
-        </div> */}
+
+          <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button">
+              <button className="btn_all text-[#545454] hover:scale-105">
+                <IoIosSettings size={iconSize} className="bg-transparent" />
+              </button>
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content z-[1] menu p-4 shadow bg-white rounded-box w-52 gap-3 "
+            >
+              <ModalChangePassword user={user} />
+              <button
+                className="flex justify-start items-center btn_all gap-2 w-full transition duration-300 ease-in-out bg-red-600 hover:bg-red-500 text-white text-sm"
+                onClick={handleLogOut}
+              >
+                <FaSignOutAlt size={20} style={iconStyle} />
+                <p className="">Sign out</p>
+              </button>
+            </ul>
+          </div>
+        </div>
       </div>
     </>
   );
--- FrontEnd/TripMates/src/components/NewsFeed/SideLeft/index.jsx
+++ FrontEnd/TripMates/src/components/NewsFeed/SideLeft/index.jsx
@@ -1,10 +1,8 @@
 import { BiSolidMoviePlay } from "react-icons/bi";
 import { FaCompass } from "react-icons/fa";
 import { AiFillMessage } from "react-icons/ai";
-
-
-
-import User from "./User";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
 // import { useEffect, useState } from "react";
 // import { useParams } from "react-router";
 // import { getUserById } from "../../../services/user";
@@ -12,37 +10,59 @@ import User from "./User";
 const LeftSideBar = () => {
   const iconSize = 20;
   const iconStyle = { background: "transparent" };
- 
-    // const [userProfile, setUserProfile] = useState({
-    //   email: "",
-    //   fullName: "",
-    //   avatar: "",
-    //   age: null,
-    //   birthday: "",
-    //   gender: "",
-    //   description: "",
-    // });
-    // const urlParam = useParams();
+  const user = useSelector((state) => state.user.user);
+  const navigate = useNavigate();
+  // const [userProfile, setUserProfile] = useState({
+  //   email: "",
+  //   fullName: "",
+  //   avatar: "",
+  //   age: null,
+  //   birthday: "",
+  //   gender: "",
+  //   description: "",
+  // });
+  // const urlParam = useParams();
 
-    // useEffect(() => {
-    //   const fetchUserData = async () => {
-    //     try {
-    //       console.log('abc' , urlParam);
-    //       const response = await getUserById(urlParam.userId);
-    //       console.log(response);
-    //       setUserProfile(response.data.findUser);
-    //     } catch (error) {
-    //       console.log(error);
-    //     }
-    //   };
-    //   fetchUserData();
-    // }, [urlParam.userId, userProfile]);
+  // useEffect(() => {
+  //   const fetchUserData = async () => {
+  //     try {
+  //       console.log('abc' , urlParam);
+  //       const response = await getUserById(urlParam.userId);
+  //       console.log(response);
+  //       setUserProfile(response.data.findUser);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   fetchUserData();
+  // }, [urlParam.userId, userProfile]);
   return (
     <>
       <div className="flex flex-col font-bold gap-[3rem] pt-[2rem] w-[85%]">
         <div className="flex flex-col gap-12">
           <div className="flex flex-col gap-5">
-           <User/>
+            <div className="flex items-center btn_all cursor-pointer gap-2" onClick={() => navigate(`/user/${user._id}`)}>
+              <div className="w-[22px] h-[22px]">
+                <img
+                  className="w-full h-full object-cover rounded-full"
+                  src={user.avatar}
+                  alt=""
+                />
+              </div>
+              <div className="flex-1 overflow-hidden">
+                <p className="truncate">{user.fullName}</p>
+                {/* <p className="text-xs text-[#717171]">{user.email}</p> */}
+              </div>
+            </div>
+
+            <button className="flex justify-start items-center btn_all gap-2 w-full focus:bg-[#303030] focus:text-white hover:bg-[#303030] hover:text-white transition duration-300 ease-in-out">
+              <FaCompass
+                className="group-hover:bg-[#303030]"
+                size={iconSize}
+                style={iconStyle}
+              />
+              <p className="">Feeds</p>
+            </button>
             <button className="flex justify-start items-center btn_all gap-2 w-full focus:bg-[#303030] focus:text-white hover:bg-[#303030] hover:text-white transition duration-300 ease-in-out">
               <FaCompass
                 className="group-hover:bg-[#303030]"
