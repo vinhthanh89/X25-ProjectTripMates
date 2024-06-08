a80aeaa8a96be712ed7d0415b9cf1dd5350a2eb4
iamchouty <quocchau4729@gmail.com>
2024-06-08 23:28:16 +0700

    FrontEnd - added Autocompleted to search places input

--- FrontEnd/TripMates/src/components/NewsFeed/TopicForm/index.jsx
+++ FrontEnd/TripMates/src/components/NewsFeed/TopicForm/index.jsx
@@ -1,8 +1,31 @@
-import { Button, DatePicker, Drawer, Form, Input } from "antd";
+import { Button, DatePicker, Drawer, Form, Input, AutoComplete } from "antd";
 import dayjs from "dayjs";
-// eslint-disable-next-line react/prop-types
+import { useState } from "react";
+
 const TopicForm = ({ onClose, open }) => {
-  const { TextArea } = Input;
+  const [places, setPlaces] = useState([]);
+
+  const staticPlaces = [
+    "China",
+    "Chiangmai",
+    "Japan",
+    "India",
+    "Thailand",
+    "Vietnam",
+  ];
+
+  const inputStyle = {
+    width: "25rem",
+    height: "3rem",
+    borderRadius: "var(--rounded-btn, 0.5rem)",
+    fontSize: "1rem",
+    fontWeight: "700",
+    lineHeight: "1.25rem",
+    borderWidth: "2px",
+    backgroundColor: "var(--fallback-b1,oklch(var(--b1)/var(--tw-bg-opacity)))",
+    flexShrink: "1",
+    appearance: "none",
+  };
 
   const onFinish = (values) => {
     const formData = {
@@ -15,118 +38,105 @@ const TopicForm = ({ onClose, open }) => {
     console.log(formData);
   };
 
+  const fetchPlaces = async (inputValue) => {
+    const filteredPlaces = staticPlaces.filter((place) =>
+      place.toLowerCase().includes(inputValue.toLowerCase())
+    );
+    setPlaces(filteredPlaces);
+  };
+
   return (
-    <>
-      <Drawer onClose={onClose} open={open} width={500}>
-        <Form name="basic" className="pb-[20px]" onFinish={onFinish}>
-          <div className="text-black px-5">
-            <h2 className="text-2xl font-semibold mb-4">Start a New Trip</h2>
-            <div className="flex flex-col gap-4">
-              <Form.Item name="title">
-                <label className="form-control max-w-xs">
-                  <div className="label">
-                    <span className="label-text font-bold text-black text-base">
-                      Topic name
-                    </span>
-                  </div>
-                  <Input className="input input-bordered border-2 border-[#d2d2d2] hover:border-[#4096ff] focus:border-[#4096ff] bg-white font-bold w-[25rem] transition-colors duration-300" />
-                </label>
-                {/* <label className="form-control max-w-xs">
-                  <div className="label">
-                    <span className="label-text font-bold text-black text-base">
-                      Topic name
-                    </span>
-                  </div>
-                  <input
-                    type="text"
-                    placeholder="e.g., Spring in Hanoi"
-                    className="input input-bordered border-2 border-[#d2d2d2] hover:border-[#4096ff] focus:border-[#4096ff] bg-white font-bold w-[25rem] transition-colors duration-300"
-                  />
-                </label> */}
-              </Form.Item>
-              <Form.Item name="description">
-                <label className="form-control max-w-xs">
-                  <div className="label">
-                    <span className="label-text font-bold text-black text-base">
-                      Description
-                    </span>
-                  </div>
-                  <TextArea
-                    showCount
-                    maxLength={400}
-                    style={{
-                      height: 120,
-                      width: 400,
-                      resize: "none",
-                      fontWeight: "700",
-                      fontSize: "1rem",
-                    }}
-                    className="border-2 border-[#d2d2d2] hover:border-[#4096ff] focus:border-[#4096ff] transition-colors duration-300"
-                  />
-                </label>
-              </Form.Item>
-              <Form.Item name="location">
-                <label className="form-control max-w-xs">
-                  <div className="label">
-                    <span className="label-text font-bold text-black text-base">
-                      Where to? (countries)
-                    </span>
-                  </div>
-                  <Input
-                    placeholder="e.g., Asia"
-                    className="input input-bordered border-2 border-[#d2d2d2] hover:border-[#4096ff] focus:border-[#4096ff] bg-white font-bold w-[25rem] transition-colors duration-300"
-                  />
-                  {/* <input
-                    type="text"
-                    placeholder="e.g., Asia"
-                    className="input input-bordered border-2 border-[#d2d2d2] hover:border-[#4096ff] focus:border-[#4096ff] bg-white font-bold w-[25rem] transition-colors duration-300"
-                  /> */}
-                </label>
-              </Form.Item>
-
-              <div className="flex justify-between">
-                <div className="form-control max-w-xs">
-                  <div className="label">
-                    <span className="label-text font-bold text-black text-base">
-                      Start date
-                    </span>
-                  </div>
-
-                  <Form.Item name="startDate">
-                    <DatePicker
-                      placeholder="e.g., 2024-01-01"
-                      className="input input-bordered border-2 border-[#d2d2d2] hover:border-[#4096ff] focus:border-[#4096ff] bg-white font-extrabold w-[12rem]"
-                      format="DD-MM-YYYY"
-                    />
-                  </Form.Item>
+    <Drawer onClose={onClose} open={open} width={500}>
+      <Form name="basic" className="pb-[20px]" onFinish={onFinish}>
+        <div className="text-black px-5">
+          <h2 className="text-2xl font-semibold mb-4">Start a New Trip</h2>
+          <div className="flex flex-col gap-4">
+            <Form.Item name="title">
+              <label className="form-control max-w-xs">
+                <div className="label">
+                  <span className="label-text font-bold text-black text-base">
+                    Topic name
+                  </span>
                 </div>
-                <div className="form-control max-w-xs">
-                  <div className="label">
-                    <span className="label-text font-bold text-black text-base">
-                      End date
-                    </span>
-                  </div>
-                  <Form.Item name="endDate">
-                    <DatePicker
-                      placeholder="e.g., 2024-01-01"
-                      className="input input-bordered border-2 border-[#d2d2d2] hover:border-[#4096ff] focus:border-[#4096ff] bg-white font-extrabold w-[12rem]"
-                      format="DD-MM-YYYY"
-                    />
-                  </Form.Item>
+                <Input className="input border-[#d2d2d2] hover:border-[#4096ff] focus:border-[#4096ff] bg-white font-bold w-[25rem] transition-colors duration-300" />
+              </label>
+            </Form.Item>
+            <Form.Item name="description">
+              <label className="form-control max-w-xs">
+                <div className="label">
+                  <span className="label-text font-bold text-black text-base">
+                    Description
+                  </span>
                 </div>
+                <Input.TextArea
+                  showCount
+                  maxLength={400}
+                  style={{
+                    height: "10rem",
+                    width: "25rem",
+                    resize: "none",
+                    fontWeight: "700",
+                    fontSize: "1rem",
+                  }}
+                  className="border-2 border-[#d2d2d2] hover:border-[#4096ff] focus:border-[#4096ff] transition-colors duration-300"
+                />
+              </label>
+            </Form.Item>
+            <Form.Item name="location">
+              <label className="form-control max-w-xs">
+                <div className="label">
+                  <span className="label-text font-bold text-black text-base">
+                    Where to? (countries)
+                  </span>
+                </div>
+                <AutoComplete
+                  options={places.map((place) => ({ value: place }))}
+                  onSearch={fetchPlaces}
+                  placeholder="e.g., Asia"
+                  style={inputStyle}
+                />
+              </label>
+            </Form.Item>
+            <div className="flex justify-between">
+              <div className="form-control max-w-xs">
+                <div className="label">
+                  <span className="label-text font-bold text-black text-base">
+                    Start date
+                  </span>
+                </div>
+                <Form.Item name="startDate">
+                  <DatePicker
+                    placeholder="e.g., 2024-01-01"
+                    className="input border-2 border-[#d2d2d2] hover:border-[#4096ff] focus:border-[#4096ff] bg-white font-extrabold w-[12rem]"
+                    format="DD-MM-YYYY"
+                  />
+                </Form.Item>
               </div>
-
-              <Button
-                htmlType="submit"
-                className="bg-[#5143d9] h-[50px] text-[20px] font-bold text-white "
-              >
-                Create trip
-              </Button>
+              <div className="form-control max-w-xs">
+                <div className="label">
+                  <span className="label-text font-bold text-black text-base">
+                    End date
+                  </span>
+                </div>
+                <Form.Item name="endDate">
+                  <DatePicker
+                    placeholder="e.g., 2024-01-01"
+                    className="input border-2 border-[#d2d2d2] hover:border-[#4096ff] focus:border-[#4096ff] bg-white font-extrabold w-[12rem]"
+                    format="DD-MM-YYYY"
+                  />
+                </Form.Item>
+              </div>
             </div>
+            <Button
+              htmlType="submit"
+              className="bg-[#5143d9] h-[50px] text-[20px] font-bold text-white"
+            >
+              Create trip
+            </Button>
           </div>
-        </Form>
-      </Drawer>
-    </>
+        </div>
+      </Form>
+    </Drawer>
   );
 };
 
