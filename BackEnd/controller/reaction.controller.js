f8bd8e8d6179c6b66875c8141c03d516772a99a2
vinhthanh89 <149566726+vinhthanh89@users.noreply.github.com>
2024-07-06 21:00:15 +0700

    fix reactions

--- BackEnd/controller/reaction.controller.js
+++ BackEnd/controller/reaction.controller.js
@@ -1,8 +1,9 @@
 import Reaction from "../model/reaction.model.js";
+import Topic from '../model/topic.model.js'
 
 export const getReactByTopicId = async (req, res) => {
   try {
-    const userId = req.user;
+    const userLogin = req.user;
     const topicId = req.params.topicId;
 
     const findReactByTopic = await Reaction.findOne({
@@ -13,13 +14,21 @@ export const getReactByTopicId = async (req, res) => {
       return res.status(200).json({
         message: "Topic has no reactions yet",
         reactions: [],
+        isReact : false
       });
     }
 
     if (findReactByTopic) {
+      const reactions = findReactByTopic.usersReaction
+      // console.log(reactions);
+      const userExists = reactions.some(
+        (reaction) => reaction.userId._id.toString() === userLogin
+      );
+
       return res.status(200).json({
         message: "Get React By Topic Successfully",
-        reactions: findReactByTopic.usersReaction
+        reactions: findReactByTopic.usersReaction,
+        isReact : userExists
       });
     }
   } catch (error) {
@@ -35,28 +44,15 @@ export const reactTopic = async (req, res) => {
     const userId = req.user;
     const topicId = req.params.topicId;
 
-    const findTopicReaction = await Reaction.findOne({
-      topicIdReaction: topicId,
-    }).populate('usersReaction usersReaction.userId');
+    const findTopic = await Topic.findById(topicId)
 
-    if (!findTopicReaction) {
-      const newTopicReactions = await Reaction.create({
-        topicIdReaction: topicId,
-        usersReaction: [
-          {
-            userId: userId,
-            react: "like",
-          },
-        ],
-      });
-
-      return res.status(201).json({
-        message: "React Successfully",
-        reactions : newTopicReactions.usersReaction
-      });
+    if(!findTopic){
+      return res.status(404).json({
+        message : 'Topic Not Found'
+      })
     }
 
-    if (findTopicReaction) {
+    if(findTopic){
       const newTopicReactions = await Reaction.findOneAndUpdate(
         { topicIdReaction: topicId },
         {
@@ -66,14 +62,15 @@ export const reactTopic = async (req, res) => {
                 react: 'like'
               }
             }
-          },{new : true}
+          },{new : true , upsert : true}
       ).populate('usersReaction usersReaction.userId');
-
+  
       return res.status(201).json({
         message: "React Successfully",
         reactions : newTopicReactions.usersReaction,
       });
     }
+
   } catch (error) {
     console.log(error);
     return res.status(500).json({
