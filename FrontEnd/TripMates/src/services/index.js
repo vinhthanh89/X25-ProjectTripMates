7ebcc1685ebb1af0a6254d628cd470d3cba000f2
vinhthanh89 <149566726+vinhthanh89@users.noreply.github.com>
2024-07-16 22:07:51 +0700

    fix API URL

--- FrontEnd/TripMates/src/services/index.js
+++ FrontEnd/TripMates/src/services/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { API_URL } from '../config'
+import { API_URL} from '../config'
 import { getAccessTokenFromLocal, getRefreshTokenFromLocal, saveAccessTokenToLocal } from '../utils/localstorage'
 
 export const axiosInstance = axios.create({
