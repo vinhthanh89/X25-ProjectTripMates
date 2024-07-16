662e772424043a8b82875057650ef9b34f8c1974
vinhthanh89 <149566726+vinhthanh89@users.noreply.github.com>
2024-07-16 21:59:23 +0700

    fix cors and API_URL

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,26 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
+import connectToDb from "./database/connectToDb.js";
+import router from "./routes/index.js";
 
-import connectToDb from './database/connectToDb.js'
-import router from './routes/index.js'
+dotenv.config();
 
+const PORT = process.env.PORT;
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    // origin : "*"
+    origin: [
+      "https://x25-projecttripmates.onrender.com/",
+      "http://localhost:5173",
+    ],
+  })
+);
+app.use(router);
 
-dotenv.config()
-
-const PORT = process.env.PORT
-const app = express()
-app.use(express.json())
-app.use(cors({
-    origin : "*"
-}))
-app.use(router)
-
-
-app.listen(PORT , () => {
-    console.log(`App is running in http://localhost:${PORT}`);
-    connectToDb()
-})
-
-
+app.listen(PORT, () => {
+  console.log(`App is running in http://localhost:${PORT}`);
+  connectToDb();
+});
